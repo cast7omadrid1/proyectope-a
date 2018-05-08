0f37232fad98936fad42b3b476f1c3642b29026f
Aaron Castro <castro1162@gmail.com>
2018-05-08 21:14:14 +0200

    'pruebarss'

--- resources/views/noticias.blade.php
+++ resources/views/noticias.blade.php
@@ -12,19 +12,18 @@
         </div>
         
         <h4 class="classic-title tituloseccion tituloredes" onmouseover="javascript:this.style.color='#00D2A8';" onmouseout="javascript:this.style.color='#262626';"><span>Calendario</span></h4>
- 		<!--prueba rss-->
-		<!-- start feedwind code -->
-		<div class="col-xs-8">
-			<div class="col-xs-6">
- 				<iframe width="400" height="400" style="border:none;" src="http://output45.rssinclude.com/output?type=iframe&amp;id=1180026&amp;hash=27fd93cc4b44950fbdaceb6a76b5a3be"></iframe>
- 			</div>
- 			<div class="col-xs-6">
- 				<script type="text/javascript" src="https://feed.mikle.com/js/fw-loader.js" data-fw-param="67710/"></script>
- 			</div>
-		</div>
-		<div class="col-xs-4">
- 			<script type="text/javascript" src="https://feed.mikle.com/js/fw-loader.js" data-fw-param="67710/"></script>
-		</div>
+
+
+		
+		<div class="panel panel-primary sombra zoomIt">
+                          <div class="panel-heading leftname">
+                            <h3 class="panel-title ">Categorias</h3>
+                          </div>
+                          <div class="panel-body">
+                            <script language="javascript" type="text/javascript" src="http://rss.sindicacion.net/view_rss/b2ZtPTQmb2Z4PXRydWU=/ZWZiPXRydWU=/aD1odHRwJTNBJTJGJTJGZXN0YXRpY29zLm1hcmNhLmNvbSUyRnJzcyUyRmZ1dGJvbCUyRnByaW1lcmEtZGl2aXNpb24ueG1s"></script><a href="http://rss.sindicacion.net/" style="padding:2px 0px 2px 8px;text-indent:-8px;font:12px Times New Roman, serif;color:#0000FF;">RSS</a>
+                          </div>
+</div>
+ 		
  	</div>
 
 @endsection
